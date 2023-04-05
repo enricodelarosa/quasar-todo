ab530ab22acae6520c2b1f0f0da779830191c640
Enrico Emil Dela Rosa <enricodelarosa01@gmail.com>
2023-04-05 22:46:32 +0800

    Initial commit

---
+++ src/pages/HelpPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas vel eius dolor! Assumenda iste reiciendis illum ab eaque neque aliquid adipisci, maiores obcaecati consectetur aperiam, tempore corporis suscipit voluptatem minus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla ipsum, eveniet sunt officia ratione numquam consequatur quaerat esse enim deleniti sapiente at, accusantium nisi quidem expedita similique debitis deserunt consectetur!</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'HelpPage'
+})
+</script>
